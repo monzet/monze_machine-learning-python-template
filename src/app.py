c0bfd018b5770e3a6e091ea8176a0349e6b7147e
juditBioE <73213105+juditBioE@users.noreply.github.com>
2025-08-05 06:06:12 +0000

    Added model to app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,32 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import joblib
+import numpy as np
 
-# your code here
+# Cargar el modelo
+with open('models/modelo_entrenado.pkl', 'rb') as file:
+    model = joblib.load('models/modelo_entrenado.pkl')
+
+
+
+st.title("Predicción de Diabetes")
+
+# Inputs del usuario
+pregnancies = st.number_input('Embarazos', min_value=0)
+glucose = st.number_input('Glucosa', min_value=0)
+blood_pressure = st.number_input('Presión arterial', min_value=0)
+skin_thickness = st.number_input('Espesor de piel', min_value=0)
+insulin = st.number_input('Insulina', min_value=0)
+bmi = st.number_input('IMC', min_value=0.0, format="%.1f")
+dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, format="%.3f")
+age = st.number_input('Edad', min_value=0)
+
+# Botón para predecir
+if st.button('Predecir'):
+    input_data = np.array([[pregnancies, glucose, blood_pressure, skin_thickness,
+                            insulin, bmi, dpf, age]])
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.error('Posible diabetes')
+    else:
+        st.success('No parece haber diabetes')
