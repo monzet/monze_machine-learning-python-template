b42ec7de212bd56dded043373213d608a634cb06
Monze <52428975+Monzet@users.noreply.github.com>
2025-08-16 19:38:51 +0000

    version final

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Cargar el modelo
+# Carga de modelo
 with open('models/modelo_entrenado.pkl', 'rb') as file:
     model = joblib.load('models/modelo_entrenado.pkl')
 
@@ -10,7 +10,7 @@ with open('models/modelo_entrenado.pkl', 'rb') as file:
 
 st.title("Predicción de Diabetes")
 
-# Inputs del usuario
+# Datos de usuario
 pregnancies = st.number_input('Embarazos', min_value=0)
 glucose = st.number_input('Glucosa', min_value=0)
 blood_pressure = st.number_input('Presión arterial', min_value=0)
@@ -20,7 +20,7 @@ bmi = st.number_input('IMC', min_value=0.0, format="%.1f")
 dpf = st.number_input('Diabetes Pedigree Function', min_value=0.0, format="%.3f")
 age = st.number_input('Edad', min_value=0)
 
-# Botón para predecir
+# Boton para predecir
 if st.button('Predecir'):
     input_data = np.array([[pregnancies, glucose, blood_pressure, skin_thickness,
                             insulin, bmi, dpf, age]])
